942c572e7cac4e05b24e1dd31eeb3f34da5d88a6
David Luposchainsky <dluposchainsky@gmail.com>
2013-03-27 23:09:56 +0100

    Architecture instance for Board almost done
    - Converted Architecture to class
    - Implemented most functions for the Board instance (getters/setters
      still missing)
    - Terminal instance still untouched

--- System/Hardware/HPIO/Architecture.hs
+++ System/Hardware/HPIO/Architecture.hs
@@ -1,9 +1,6 @@
-
-
 module System.Hardware.HPIO.Architecture (
-        defaultArchitecture
-      , Architecture(..)
-      , Pins(..)
+      Pins(..),
+      Architecture(..)
 ) where
 
 
@@ -25,67 +22,46 @@ newtype Pins uid = Pins (IORef (Map (UID uid) Pin))
 
 -- | An architecture unifies all the GPIO pin functions of a certain device,
 --   for example manipulating GPIO on a Raspberry Pi, or in a terminal.
-data Architecture uid = Architecture {
+class Architecture a where
 
       -- | Construct a new architecture, containing only an empty record.
-        construct       :: IO (Pins uid)
+      construct :: a -> IO (Pins uid)
+      construct a = Pins <$> newIORef Map.empty
 
       -- | Deallocate all allocated pins by calling 'removePin' on them.
-      , destruct        :: Pins uid -> IO ()
+      destruct :: a -> Pins uid -> IO ()
 
       -- | Deallocate all pins, ignoring exceptions. Useful for bracketing, so
       --   that the pins are released no matter what when the program finishes.
-      , nuke            :: Pins uid -> IO ()
+      nuke :: a -> Pins uid -> IO ()
 
       -- | Adds a new pin to the system.
-      , addPin          :: Pins uid -> HWID -> UID uid -> PinDirection -> IO ()
+      addPin :: (Ord uid) => a -> Pins uid -> HWID -> UID uid -> PinDirection -> IO ()
 
       -- | Incorporates an already open pin into the system.
-      , absorbPin       :: Pins uid -> HWID -> UID uid -> PinDirection -> IO ()
+      absorbPin :: (Ord uid) => a -> Pins uid -> HWID -> UID uid -> PinDirection -> IO ()
 
       -- | Deallocates a pin from the system.
-      , removePin       :: Pins uid -> UID uid -> IO ()
+      removePin :: (Ord uid) => a -> Pins uid -> UID uid -> IO ()
 
       -- | Checks whether a pin is currently allocated in the 'Pins' object.
       --   Note that this does /not/ check whether the pin is present on an OS
       --   level, only whether this 'Pins' knows about it.
-      , isOpen          :: Pins uid -> UID uid -> IO Bool
+      isOpen :: (Ord uid) => a -> Pins uid -> UID uid -> IO Bool
 
-      -- | Checks whether a pin is constructable, i.e. has not been allocated
-      --   yet on the system level.
-      , isConstructable :: Pins uid -> HWID -> IO Bool
+      -- | Checks whether a pin is constructable, i.e. is neither open on a
+      --   hardware level nor known to the current 'Pins'. Success means that
+      --   "construct" should not yield an error.
+      isConstructable :: (Ord uid) => a -> Pins uid -> UID uid -> HWID -> IO Bool
 
       -- | Updates the value of a pin.
-      , setPinValue     :: Pins uid -> UID uid -> PinValue -> IO ()
+      setPinValue :: a -> Pins uid -> UID uid -> PinValue -> IO ()
 
       -- | Gets the value of a pin.
-      , getPinValue     :: Pins uid -> UID uid -> IO PinValue
+      getPinValue :: a -> Pins uid -> UID uid -> IO PinValue
 
       -- | Sets the direction of a pin.
-      , setPinDirection :: Pins uid -> UID uid -> PinDirection -> IO ()
+      setPinDirection :: a -> Pins uid -> UID uid -> PinDirection -> IO ()
 
       -- | Gets the direction of a pin.
-      , getPinDirection :: Pins uid -> UID uid -> IO PinDirection
-
-}
-
-defaultConstruct :: IO (Pins uid)
-defaultConstruct = Pins <$> newIORef Map.empty
-
-
-
-defaultArchitecture :: Architecture uid
-defaultArchitecture = Architecture {
-        construct       = defaultConstruct
-      , destruct        = undefined
-      , nuke            = undefined
-      , addPin          = undefined
-      , absorbPin       = undefined
-      , removePin       = undefined
-      , isOpen          = undefined
-      , isConstructable = undefined
-      , setPinValue     = undefined
-      , getPinValue     = undefined
-      , setPinDirection = undefined
-      , getPinDirection = undefined
-}
+      getPinDirection :: a -> Pins uid -> UID uid -> IO PinDirection
--- System/Hardware/HPIO/Architecture/Board.hs
+++ System/Hardware/HPIO/Architecture/Board.hs
@@ -6,7 +6,7 @@ import System.Hardware.HPIO.BasicPin
 import System.Hardware.HPIO.PinID
 
 
-import System.Hardware.HPIO.Architecture (Architecture, Pins(Pins))
+import System.Hardware.HPIO.Architecture (Pins(Pins), Architecture)
 import qualified System.Hardware.HPIO.Architecture as A
 
 import Text.Printf
@@ -15,23 +15,25 @@ import qualified Data.Map as Map
 import Data.Functor
 import Control.Monad
 import Data.Traversable
+import Data.Maybe
+import Control.Applicative
 
 
+newtype Board uid = Board uid
 
-board :: Architecture uid
-board = A.defaultArchitecture {
-        A.destruct        = destruct
-      , A.nuke            = nuke
-      , A.addPin          = addPin
-      , A.absorbPin       = undefined
-      , A.removePin       = undefined
-      , A.isOpen          = undefined
-      , A.isConstructable = undefined
-      , A.setPinValue     = undefined
-      , A.getPinValue     = undefined
-      , A.setPinDirection = undefined
-      , A.getPinDirection = undefined
-}
+
+instance (Ord uid) => Architecture (Board uid) where
+      destruct        _board = destruct
+      nuke            _board = nuke
+      addPin          _board = addPin
+      absorbPin       _board = absorbPin
+      removePin       _board = removePin
+      isOpen          _board = isOpen
+      isConstructable _board = isConstructable
+      setPinValue     _board = TODO
+      getPinValue     _board = TODO
+      setPinDirection _board = TODO
+      getPinDirection _board = TODO
 
 -- | Sequences an IO action over all pins contained in a Pins object. Used as
 --   a common interface for "destruct" and "nuke".
@@ -44,17 +46,49 @@ destruct = sequenceAllPins Mid.destruct
 nuke :: Pins uid -> IO ()
 nuke = sequenceAllPins Mid.nuke
 
-addPin :: (Ord uid) => Pins uid-> HWID-> UID uid-> PinDirection-> IO ()
-addPin (Pins pinsRef) hwid uid dir = do
+addPin :: (Ord uid) => Pins uid -> HWID -> UID uid -> PinDirection -> IO ()
+addPin (Pins pRef) hwid uid dir = do
       -- Check existence
       ex <- Mid.exists hwid
       when ex $ error "Pin already allocated on the hardware"
       -- Check previous allocation
-      p <- readIORef pinsRef
+      p <- readIORef pRef
       when (uid `Map.member` p) $ error "Pin already in the pins list"
       -- Everything fine, allocate
       newPin <- Mid.construct hwid dir
-      writeIORef pinsRef $ Map.insert uid newPin p
+      writeIORef pRef $ Map.insert uid newPin p
+
+absorbPin :: (Ord uid) => Pins uid -> HWID -> UID uid -> PinDirection -> IO ()
+absorbPin (Pins pRef) hwid uid dir = do
+      -- Check existence
+      ex <- Mid.exists hwid
+      when (not ex) $ error "Pin not allocated on the hardware, cannot absorb"
+      -- Check previous allocation
+      p <- readIORef pRef
+      when (uid `Map.member` p) $ error "Pin already in the pins list"
+      -- Everything fine, absorb
+      newPin <- Mid.absorb hwid
+      writeIORef pRef $ Map.insert uid newPin p
+
+removePin :: (Ord uid) => Pins uid -> UID uid -> IO ()
+removePin (Pins pRef) uid = do
+      p <- readIORef pRef
+      case Map.lookup uid p of
+            Just pin -> do Mid.destruct pin
+                           writeIORef pRef (Map.delete uid p)
+            Nothing  -> error "Pin not in pins list"
+
+isOpen :: (Ord uid) => Pins uid -> UID uid -> IO Bool
+isOpen (Pins pRef) uid = readIORef pRef >>= return . isJust . Map.lookup uid
+
+isConstructable :: (Ord uid) => Pins uid -> UID uid -> HWID -> IO Bool
+isConstructable pins uid hwid = liftA2 nor open ex
+      where nor x y = not (x && y)
+            open    = isOpen pins uid
+            ex      = Mid.exists hwid
+
+
+
 
 --construct :: HWID -> PinDirection -> IO Pin
 
@@ -63,25 +97,6 @@ addPin (Pins pinsRef) hwid uid dir = do
 --newtype Pins uid = Pins (IORef (Map (UID uid) Pin))
 
 
-
-
---nuke :: Pins uid -> IO ()
---nuke pins = void $ Map.map Mid.nuke <$> readIORef pins
---
---
---
---addPin :: (Show uid, Ord uid)
---       => Pins uid
---       -> HWID
---       -> UID uid
---       -> PinDirection
---       -> IO ()
---addPin pins hwid uid dir = do
---      pins <- readIORef pins
---      if Map.member uid pins
---            then error $ printf "Pin %s already exists" (show uid)
---            else do newPin <- Mid.construct hwid dir
---                    writeIORef pins $ Map.insert uid newPin pins
 
 --addHiPin :: (Show uid, Ord uid)
 --         => HiPin
